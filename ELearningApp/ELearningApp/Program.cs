52a6d284e34f4223fde1b57759c11fe67759e176
chris samuel <cosalcedo@up.edu.ph>
2025-07-21 14:27:38 +0800

    dev/remove seeding

--- ELearningApp/ELearningApp/Program.cs
+++ ELearningApp/ELearningApp/Program.cs
@@ -122,19 +122,6 @@ app.MapAdditionalIdentityEndpoints();
 // Seed database with sample data in development
 if (app.Environment.IsDevelopment())
 {
-    using (var scope = app.Services.CreateScope())
-    {
-        try
-        {
-            var seeder = scope.ServiceProvider.GetRequiredService<DatabaseSeeder>();
-            await seeder.SeedAsync();
-        }
-        catch (Exception ex)
-        {
-            var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
-            logger.LogError(ex, "An error occurred while seeding the database.");
-        }
-    }
 }
 
 app.Run();
